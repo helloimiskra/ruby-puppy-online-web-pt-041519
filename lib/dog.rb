eee0654a3e0bedf454e472d229906aa626555d25
Iskra B <ibatistapoblete@gmail.com>
2019-05-16 03:43:52 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,11 +1,17 @@
 class Dog
   attr_accessor :name
-
+  
   @@all = []
+
   def initialize(name)
     @name = name
     @@all << self
   end
 
+  def self.clear_all
+    @@all = []
+  end
 
+
+    
 end
